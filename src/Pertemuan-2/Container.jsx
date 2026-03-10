9e53d2654193906f129e7193d9fb04a2e99a47d7
Azizah24sie <azizah24si@mahasiswa.pcr.ac.id>
2026-03-10 15:24:38 +0700

    Pertemuan2

---
+++ src/Pertemuan-2/Container.jsx
@@ -0,0 +1,15 @@
+export default function Container({children}){
+    return(
+        <div>
+            <h1>Pemrograman Framework Lanjutan</h1>
+            <br/>
+             <img src="img/pixel.webp" width="100%/>" alt="gambar pixel"/>
+                {children}
+            <br/>
+            
+            <footer>
+                <p>2025 - Politeknik Caltex Riau</p>
+            </footer>
+        </div>
+    )
+}
---
+++ src/Pertemuan-2/HelloWorld.jsx
@@ -0,0 +1,50 @@
+export default function HelloWorld(){
+      const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Selamat Belajar ReactJs</p>
+            <GreetingBinjai/>
+            <QuoteText/>
+            <UserCard nama="Hasbi" 
+            nim="123456789" 
+            tanggal="20 Juni 2024"/> 
+            <UserCard {...propsUserCard}/>
+        </div>
+    )
+}
+
+function GreetingBinjai(){
+    return (
+        <small className="card">Salam dari binjai 👍</small>
+    )
+}
+
+function UserCard(props){
+    return (
+        <div className="card">
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+
+        
+    )
+}
+
+function QuoteText() {
+    const text = "Mulutmu Harimaumu";
+    const text2 = "Aku ingin jadi macan";
+    return (
+        <div>
+            <hr/>
+            <p>{text.toLowerCase()}</p>
+            <p>{text2.toUpperCase()}</p>
+        </div>
+    )
+}
